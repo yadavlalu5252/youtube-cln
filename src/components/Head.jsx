caf7b5bf27f34a691cee9e922ab7ad3e7248cc5c
yadavlalu5252 <yadavlalu5252@gmail.com>
2025-06-16 12:00:33 +0530

    Header and body created

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,8 +1,21 @@
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaUser } from "react-icons/fa";
 
 const Head = () => {
     return (
-        <div>
-            <h1>head</h1>
+        <div className="grid grid-flow-col m-2 p-5 shadow-lg text-center items-center bg-white">
+            <div className="flex col-span-1">
+                <GiHamburgerMenu  className="h-8"/>
+                <img src="https://img.favpng.com/24/10/0/youtube-premium-logo-png-favpng-gydaJGF28XSHPNpyCsymNv8RM.jpg" alt="yt-logo" 
+                className="h-8 mx-2" />
+            </div>
+            <div className="col-span-10 text-center">
+                <input type="text" className="w-3/4 border border-gray-400 rounded-sm rounded-l-full p-2" />
+                <button className="border border-gray-400  rounded-r-full py-2 px-5  bg-gray-200">🔍</button>
+            </div>
+            <div className="h-8 col-span-1">
+                <FaUser />
+            </div>
         </div>
     )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,30 @@
 
 const Sidebar = () => {
     return (
-        <div>
-            <h1>Sidebar</h1>
+        <div className="p-5 shadow-lg w-1/10 border border-gray-200">
+            
+            <ul>
+                <li>Home</li>
+                <li>Shorts</li>
+                <li>Subscription</li>
+                <li>Live</li>
+            </ul>
+            <h1 className="font-bold pt-5">You</h1>
+            <ul className="">
+                <li>History</li>
+                <li>Playlist</li>
+                <li>Watch Later</li>
+                <li>Liked Vidoes</li>
+                <li>Setting</li>
+            </ul>
+            <h1 className="font-bold pt-5">Entertainment</h1>
+            <ul>
+                <li>Music</li>
+                <li>Gaming</li>
+                <li>News</li>
+                <li>Sports</li>
+                <li>Movies</li>
+            </ul>
         </div>
     )
 }
